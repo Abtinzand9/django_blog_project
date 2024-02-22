4cde490f0d1a146528daea933a0e2ecbffc245d9
DESKTOP-LA12F0T\Abtin <abtinzand9@gmail.com>
2024-02-22 18:04:38 +0330

    create blog model

--- blog project/blog/settings.py
+++ blog project/blog/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from environs import Env
 
 env = Env()
-env.read_env(path="../.gitignore/.env")
+env.read_env(path=".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
 ]
 
 MIDDLEWARE = [
---
+++ blog project/posts/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+
+class Article(models.Model):
+    """a class to make articles"""
+
+    # make the status choices
+    STATUS_OF_ARTICLES = (
+        ('checking' , 'Checking'),
+        ('rejected' , 'Rejected'),
+        ('published' , "Published")
+    )
+
+    title = models.CharField(max_length =50)
+    body = models.TextField()
+    author = models.ForeignKey(User , on_delete=models.CASCADE , related_name = 'Articles')
+    create_date = models.DateTimeField(auto_now_add = True)
+    modify_date = models.DateTimeField(default = timezone.now)
+    status = models.CharField(max_length = 15 ,choices = STATUS_OF_ARTICLES , default = 'checking')
+
+    class Meta:
+        ordering = ("create_date",)
+
+    def __str__(self):
+        """method returns title"""
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("posts_detail", kwargs={"pk": self.pk})
+    
